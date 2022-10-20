46a010f699e79efa2e189896b83e1958a7a1a77e
André Dietrich <andredietrich@web.de>
2022-10-20 11:19:36 +0200

    First demo of LiaScript VSCode Web-Extension

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# liascript-preview-vscode-web
-A VSCode Preview Web-Extension for LiaScript 
+# liascript-preview-web
+
+
+This is a preview module for [LiaScript](https://LiaScript.github.io) courses developed in Markdown.
+
+## Usage
+
+After installing press <kbd>F1</kbd> to open the fuzzy search and type "liascript", three options will be presented:
+
+* __LiaScript: Toggle Preview [<kbd>Alt</kbd> <kbd>L</kbd>]__
+
+  This will open a panel on the right side (or close it), which will be updated whenever the Markdown document is stored.
+  That means, if autosaving is activated, the document will be updated every time this happens or if you manually store the document via [<kbd>Ctrl</kbd> <kbd>S</kbd>]. 
+
+* __LiaScript: Toggle Preview in JIT mode [<kbd>Shift</kbd> <kbd>Alt</kbd> <kbd>L</kbd>]__
+
+  The difference between this and the upper mode is, that with this option a faster update mode (Just-In-Time compilation) will be used, which will only compile the differences between the current and the previous version.
+  This results in optimized update times, especially for large documents, but it also may result in glitches, when state changes of quizzes, code, etc. might be involved.
+  If this happens, you simply have to reload the Preview, which will result in an entire re-interpretation of the entire document.
+
+* __LiaScript: Reload Preview [<kbd>Alt</kbd> <kbd>F5</kbd>]__
+
+  Reload the entire document, all states are reset.
+
+
+As an alternative you can also use the keyboard shortcuts directly.
+
+## Shortcomings
+
+Multimedia content cannot be loaded directly from the editor at the moment.
+You have to commit them first to your repository and set a `base` in the following format.
+
+``` markdown
+<!--
+...
+base: https://raw.githubusercontent.com/USER/REPOSITORY/BRANCH/
+...
+-->
+
+# Main Document
+```
+
+This means, the following README - URL:
+
+`https://github.com/LiaScript/docs/blob/master/README.md`
+
+has a raw-base of:
+
+`https://raw.githubusercontent.com/LiaScript/docs/master/`
---
+++ src/web/extension.ts
@@ -0,0 +1,273 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode'
+
+const preview: {
+  [key: string]: {
+    webview: vscode.WebviewPanel
+    jit: boolean
+  }
+} = {}
+
+function compile(document: vscode.TextDocument) {
+  if (preview[document.fileName]) {
+    preview[document.fileName].webview.webview.postMessage({
+      cmd: 'compile',
+      param: document.getText(),
+    })
+  }
+}
+
+vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+  if (document && !preview[document.fileName].jit) {
+    compile(document)
+  }
+})
+
+vscode.workspace.onDidChangeTextDocument((event) => {
+  if (
+    event.document &&
+    preview[event.document.fileName] &&
+    preview[event.document.fileName].jit
+  ) {
+    preview[event.document.fileName].webview.webview.postMessage({
+      cmd: 'jit',
+      param: event.document.getText(),
+    })
+  }
+})
+
+vscode.languages.registerCodeActionsProvider('markdown', {
+  provideCodeActions(document, pos, tok) {
+    if (preview[document.fileName]) {
+      const line = pos.start.line
+
+      if (line) {
+        try {
+          preview[document.fileName].webview.webview.postMessage({
+            cmd: 'gotoLine',
+            param: line + 1,
+          })
+        } catch (e) {}
+      }
+    }
+
+    return undefined
+  },
+})
+
+function createPreview(
+  jit: boolean,
+  context: vscode.ExtensionContext,
+  editor?: vscode.TextEditor
+) {
+  if (!editor) {
+    vscode.window.showWarningMessage(
+      'LiaScript can only open Markdown files. Your file has to end with the file extension (.md).'
+    )
+
+    return
+  }
+
+  const fileName = editor.document.fileName
+  const fileEnding = fileName.split('.').slice(-1)[0]?.toLowerCase()
+
+  if (fileEnding !== 'md') {
+    vscode.window.showWarningMessage(
+      'LiaScript can only open Markdown files. Your file has to end with the file extension (.md).'
+    )
+
+    return
+  }
+
+  if (preview[fileName]) {
+    // close the panel
+    preview[fileName].webview.dispose()
+    delete preview[fileName]
+  } else {
+    // create a new preview
+    const webviewPanel = vscode.window.createWebviewPanel(
+      'vscodeTest',
+      'LiaScript Preview: ' + fileName.slice(1),
+      vscode.ViewColumn.Beside,
+      {
+        enableScripts: true,
+        retainContextWhenHidden: true,
+        //localResourceRoots: [vscode.workspace. editor.document.uri],
+      }
+    )
+
+    webviewPanel.onDidDispose(() => {
+      delete preview[fileName]
+    })
+
+    webviewPanel.webview.onDidReceiveMessage(
+      (message) => {
+        switch (message.cmd) {
+          case 'ready': {
+            webviewPanel.webview.postMessage({
+              cmd: 'compile',
+              param: editor.document.getText(),
+            })
+            break
+          }
+          case 'lineGoto': {
+            const range = editor.document.lineAt(message.param).range
+            editor.selection = new vscode.Selection(range.start, range.end)
+            editor.revealRange(range)
+            break
+          }
+          case 'eval': {
+            try {
+              eval(message.param)
+            } catch (e) {
+              console.warn('eval Error:', e)
+            }
+            break
+          }
+          default: {
+            console.warn('liascript-preview: unknown message ->', message)
+          }
+        }
+
+        return
+      },
+      undefined,
+      context.subscriptions
+    )
+
+    setHtmlContent(context.extensionUri, webviewPanel.webview)
+
+    preview[fileName] = {
+      webview: webviewPanel,
+      jit: jit,
+    }
+  }
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "liascript-preview-web" is now active!'
+  )
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('liascript-preview-web.reload', () => {
+      const editor = vscode.window.activeTextEditor
+
+      if (editor && preview[editor.document.fileName]) {
+        compile(editor.document)
+      }
+    })
+  )
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('liascript-preview-web.toggle', () => {
+      const editor = vscode.window.activeTextEditor
+
+      createPreview(false, context, editor)
+    })
+  )
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('liascript-preview-web.toggle-jit', () => {
+      const editor = vscode.window.activeTextEditor
+
+      createPreview(true, context, editor)
+    })
+  )
+}
+
+function setHtmlContent(extensionUri: vscode.Uri, webview: vscode.Webview) {
+  const liascriptPath = vscode.Uri.joinPath(
+    extensionUri,
+    'liascript',
+    'index.html'
+  )
+
+  let htmlContent = `<!DOCTYPE html>
+	  <html lang="en">
+	  <head>
+		<meta charset="UTF-8">
+	
+		<title>LiaScript - Preview</title>
+		
+		<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+		
+
+		<script>
+		const vscode = acquireVsCodeApi();
+
+		var lia;
+
+		function sendToLia(cmd, param) {
+			if (lia) {
+				lia.postMessage({cmd, param}, "*")
+			}
+			else {
+				init()
+			}
+		}
+
+		window.addEventListener("message", (event) => {
+			switch (event.data.cmd) {
+				case "jit":
+					sendToLia("jit", event.data.param)
+					break;
+				case "compile":
+					sendToLia("compile", event.data.param)
+					break;
+        case "lineGoto":
+          publish("lineGoto", event.data.param)
+          break;
+        case "gotoLine":
+          sendToLia("goto", event.data.param)
+          break
+        case "eval":
+          publish("eval", event.data.param)
+          break
+				default:
+					break;
+			}
+		  }, false);
+	
+		function publish(cmd, param) {
+		  // Call back to the extension context to save the image to the workspace folder.
+		  vscode.postMessage({ cmd, param });
+		}
+	
+		function init() {
+		  const iframe = document.getElementById("lia")
+		  
+		  if (iframe) {
+			lia = iframe.contentWindow || iframe.contentDocument;
+
+			sendToLia("eval", \`window.LIA.lineGoto = (line) => { parent.postMessage({cmd: 'lineGoto', param: line}, "*") }\`)
+
+			publish("ready", true)
+		  }
+		}
+	
+		</script>
+	  </head>
+	  <body style="padding: 0px; height: 100vh; overflow: hidden; width: 100%">
+		<iframe
+			id="lia"
+			sandbox="allow-scripts allow-same-origin allow-forms allow-pointer-lock allow-downloads"
+			allow="clipboard-read; clipboard-write;"
+			class="webview ready"
+			src="${liascriptPath}?#1"
+			style="width: 100%; height: 100%; border: 0px"
+			onload="init(this)">
+		</iframe>
+	  </body>
+	  </html>`
+
+  webview.html = htmlContent
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
